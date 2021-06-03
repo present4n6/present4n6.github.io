77c723422bc4a06d123ff85a9703c139f57146f2
present4n6 <phj971002@gmail.com>
2021-06-04 01:59:00 +0900

    test

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210604.0126';
+const cacheName = 'chirpy-20210604.0158';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
